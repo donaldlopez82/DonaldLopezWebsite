8370e5b462610f3a6739d3464319039b06754ad0
donaldlopez82 <donaldlopez@donaldlopezart.com>
2022-03-23 00:21:17 -0700

    trying to get my JS done

---
+++ P0/website/pages/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const User = require('./model/user')
+
+mongoose.connect('mongodb://localhost:27017/login-app-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+const app = express()
+app.use('/', express.static(path.join(__dirname, 'static')))
+app.use(express.json())
+
+app.post('/api/register', async (req, res) => {
+    console.log(req.body)
+    res.json({ status: 'ok' })
+})
+
+app.listen(9999, () => {
+    console.log('Server up at 9999')
+
+})
